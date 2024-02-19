674476990ccce708f2c1751cc290916b8816a2a6
Raju Sharma <rs1248560@gmail.com>
2024-02-19 23:48:19 +0530

    completed backend

--- backend/routes/postRoute.js
+++ backend/routes/postRoute.js
@@ -6,31 +6,31 @@ const { v4: uuid } = require('uuid')
 const path = require('path')
 const fs = require('fs')
 const userModel = require('../Models/userModel')
-const  mongoose  = require('mongoose')
+const mongoose = require('mongoose')
 
 
 
-    // GET : /api/posts
-    // get all posts
+// GET : /api/posts
+// get all posts
 route.get("/", async(req, res) => {
     try {
-        const allposts =await postModel.find().sort({ upadateAt: -1 }) // deascending to ascending 
-       if(allposts.length==0){
-        return res.status(404).json({
-          success : false,
-          message :'data not found'  
-        })
-       }
-       return res.status(201).json({
-        success :true,
-        allposts
+        const allposts = await postModel.find().sort({ upadateAt: -1 }) // deascending to ascending 
+        if (allposts.length == 0) {
+            return res.status(404).json({
+                success: false,
+                message: 'data not found'
+            })
+        }
+        return res.status(201).json({
+            success: true,
+            allposts
         })
     } catch (error) {
         return res.status(400).json({
-            success : false,
-            message :"Internal server error"
+            success: false,
+            message: "Internal server error"
         })
-        
+
     }
 })
 
@@ -45,7 +45,7 @@ route.post("/", authMiddleware, async(req, res) => {
         const { thumbnail } = req.files;
         let fileName = thumbnail.name;
         let splitFilename = fileName.split(".")
-        let newFileName = splitFilename[0] + uuid()  + splitFilename[1]
+        let newFileName = splitFilename[0] + uuid() + splitFilename[1]
         thumbnail.mv(path.join(__dirname, "..", "/uploads", newFileName), async(err) => {
             if (err) {
                 return res.status(500).json({ success: false, message: "Failed to upload image" })
@@ -57,7 +57,7 @@ route.post("/", authMiddleware, async(req, res) => {
                     thumbnail: newFileName,
                     creator: req.user._id
                 })
-                
+
                 console.log(newPost)
                 if (!newPost) {
                     return res.status(500).json({ success: false, message: "Failed to create post" })
@@ -80,21 +80,21 @@ route.post("/", authMiddleware, async(req, res) => {
 // GET : api/posts/:id
 route.get("/:id", async(req, res) => {
     try {
-      if(!mongoose.Types.ObjectId.isValid(req.params.id)){
-        return res.status(404).json({success:false,message:"Invalid Post Id"})
-      }
-      const post = await postModel.findById(req.params.id).populate('creator')
-      return res.status(200).json({ success: true, post })
+        if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+            return res.status(404).json({ success: false, message: "Invalid Post Id" })
+        }
+        const post = await postModel.findById(req.params.id).populate('creator')
+        return res.status(200).json({ success: true, post })
     } catch (error) {
         return res.status(500).json({ success: false, message: "Internal Server Error" })
-        
+
     }
 })
 
 // GET  : api/posts/categories/:category
 route.get("/categories/:category", async(req, res) => {
-    const {category} = req.params
-    const rawCategory = await postModel.find({category : category}).sort({createdAt: -1})
+    const { category } = req.params
+    const rawCategory = await postModel.find({ category: category }).sort({ createdAt: -1 })
     return res.status(200).json({ success: true, rawCategory })
 })
 
@@ -102,11 +102,11 @@ route.get("/categories/:category", async(req, res) => {
 
 route.get("/users/:id", async(req, res) => {
     try {
-        const {id} = req.params
-        const userData = await  postModel.find({creator : id}).sort({createdAt: -1})
+        const { id } = req.params
+        const userData = await postModel.find({ creator: id }).sort({ createdAt: -1 })
         return res.status(200).json({ success: true, userData })
     } catch (error) {
-            return res.status(500).json({ success: false, message: "Internal Server Error" })
+        return res.status(500).json({ success: false, message: "Internal Server Error" })
     }
 })
 
@@ -115,38 +115,37 @@ route.get("/users/:id", async(req, res) => {
 
 route.patch("/posts/:id", authMiddleware, async(req, res) => {
     try {
-        const {id} = req.params;
-        const {title,category,description} = req.body
-        if(!title || !category || !description){
+        const { id } = req.params;
+        const { title, category, description } = req.body
+        if (!title || !category || !description) {
             return res.status(400).json({ success: false, message: "Please fill all the fields" })
         }
-        if(!req.files){
-            const singleValue = await postModel.findByIdAndUpdate(id,{title,category,description},{new:true})
-        }
-        else{
+        if (!req.files) {
+            const singleValue = await postModel.findByIdAndUpdate(id, { title, category, description }, { new: true })
+        } else {
             const oldPost = await postModel.findById(id);
-            fs.unlink(path.join(__dirname,"..",oldPost.thumbnail),async(err)=>{
-                if(err){
+            fs.unlink(path.join(__dirname, "..", oldPost.thumbnail), async(err) => {
+                if (err) {
                     return res.status(401).json({
                         success: false,
-                        message : "Internal Server Error"
+                        message: "Internal Server Error"
                     })
                 }
-                const {thumbnail} = req.files;
+                const { thumbnail } = req.files;
                 const fileName = thumbnail.name;
-                const splitFileName =  fileName.split(".")
-                const newFileName = splitFileName[0] + uuid() +".."+ splitFileName[splitFileName.length-1]
-                thumbnail.mv(path.join(__dirname,"..","uploads",newFileName),async(err)=>{
-                    if(err){
-                        return  res.status(401).json({
-                            success : false,
-                            message : "Internal Server Error"
+                const splitFileName = fileName.split(".")
+                const newFileName = splitFileName[0] + uuid() + ".." + splitFileName[splitFileName.length - 1]
+                thumbnail.mv(path.join(__dirname, "..", "uploads", newFileName), async(err) => {
+                    if (err) {
+                        return res.status(401).json({
+                            success: false,
+                            message: "Internal Server Error"
                         })
                     }
-                    const uploadPost = await postModel.findByIdAndUpdate(id,{title,category,description,thumbnail:newFileName},{new:true})
+                    const uploadPost = await postModel.findByIdAndUpdate(id, { title, category, description, thumbnail: newFileName }, { new: true })
                     return res.status(200).json({
-                        success : true,
-                        message : "Post Updated Successfully",
+                        success: true,
+                        message: "Post Updated Successfully",
                     })
                 })
             })
@@ -155,14 +154,57 @@ route.patch("/posts/:id", authMiddleware, async(req, res) => {
         return res.status(200).json({ success: true, singleValue })
     } catch (error) {
         return res.status(500).json({ success: false, message: "Internal Server Error" })
-        
+
     }
 })
 
 // delete : api/posts/:id
 
 route.delete("/posts/:id", authMiddleware, async(req, res) => {
-    return res.send("hello from user route")
+    try {
+        const { id } = req.params
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid id "
+            })
+        }
+        if (!req.files) {
+            const post = await postModel.findByIdAndDelete(id)
+            return res.status(200).json({
+                message: 'Post Delete succesfully',
+                success: true
+            })
+        } else {
+            const findPost = await postModel.findById(id);
+            const fileName = findPost.thumbnail;
+            fs.unlink(path.join(__dirname, "..", "uploads", fileName), async(err) => {
+                if (err) {
+                    return res.status(500).json({
+                        success: false,
+                        message: "Internal Server Error"
+                    })
+                } else {
+                    await postModel.findByIdAndDelete(id)
+                        // find the post and reduce the post count from user model 
+                    const user = await userModel.findById(req.user._id)
+                    const userPostCount = user.posts - 1
+                    await userModel.findByIdAndUpdate(req.user._id, { posts: userPostCount }, { new: true })
+
+                }
+            })
+        }
+        return res.status(200).json({
+            messgae: ` post delete successfuly ${id}`,
+            success: true
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        })
+    }
+
 })
 
 
